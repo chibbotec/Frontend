a1230e589bcace2841d09fcd9725d1796f119aba
kknaks <benesia93@naver.com>
2025-05-24 13:40:13 +0900

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'react-day-picker/src/style.css';
 import './index.css'
 import App from './App.tsx'
-import 'react-day-picker/src/style.css';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
